ff16bce83de3da04bb17ef75a6fa7811507f1559
Filip Płaczek <filip.alpha@expertido.com>
2022-11-14 15:51:57 +0100

    cleared project sketch

--- prz-project-esp32-read-data-module/src/main.cpp
+++ prz-project-esp32-read-data-module/src/main.cpp
@@ -1,16 +1,5 @@
 #include <Arduino.h>
 
-
-#define RPM_INPUT A7
-#define OIL_TEMP_INPUT A6
-#define OIL_TEMP_OUTPUT A10
-
-void pinInitialize(){
-pinMode(RPM_INPUT, INPUT);
-pinMode(OIL_TEMP_INPUT, INPUT);
-pinMode(OIL_TEMP_OUTPUT, OUTPUT);
-}
-
 void setup() {
   // put your setup code here, to run once:
 }
