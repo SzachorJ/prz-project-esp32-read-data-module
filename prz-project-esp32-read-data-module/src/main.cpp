ee47b7e5c7e2fbf73b1ebe86617a1adca8fbea67
Filip Płaczek <filip.alpha@expertido.com>
2022-11-24 20:53:27 +0100

    [read-data-1] defined pins, added pinSetup function

--- prz-project-esp32-read-data-module/src/main.cpp
+++ prz-project-esp32-read-data-module/src/main.cpp
@@ -1,9 +1,27 @@
 #include <Arduino.h>
 
-void setup() {
+#define V_S 36
+#define V_C 37
+#define RPM_S 38
+#define RPM_C ...
+#define OIL_TEMP_OUT 1
+
+void pinSetUp()
+{
+  pinMode(V_S, INPUT);
+  pinMode(OIL_TEMP_OUT, OUTPUT);
+}
+
+void setup()
+{
+
+  pinSetUp();
   // put your setup code here, to run once:
 }
 
-void loop() {
+void loop()
+{
+
+  Serial.print("ok");
   // put your main code here, to run repeatedly:
 }
