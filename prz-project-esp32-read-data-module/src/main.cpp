7fce780d5254c0c9c0c66cee76af2d86f3e36a54
Filip Płaczek <filip.alpha@expertido.com>
2022-11-24 20:35:09 +0100

    testtest

--- prz-project-esp32-read-data-module/src/main.cpp
+++ prz-project-esp32-read-data-module/src/main.cpp
@@ -1,9 +1,20 @@
 #include <Arduino.h>
+#include <analog_write.h>
 
-void setup() {
+#define pin_a 8
+
+const int pwm = 123;
+
+void setup()
+{
+
+  pinMode(pin_a, OUTPUT);
   // put your setup code here, to run once:
 }
 
-void loop() {
+void loop()
+{
+
+  analogWrite(pin_a, pwm);
   // put your main code here, to run repeatedly:
 }
